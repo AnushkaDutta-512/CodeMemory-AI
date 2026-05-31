9d4f98e6eedfa66b37e8942daec6b0e5121c31fc
Anushka Dutta <anushka.23FE10CSE00399@muj.manipal.edu>
2026-05-31 17:53:10 +0530

    Enabled CORS support for frontend integration

--- api.py
+++ api.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from fastapi.middleware.cors import CORSMiddleware
 from readmeforge import RepoAnalyzer
 
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class RepoRequest(BaseModel):
 
